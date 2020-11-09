f770e416ea88449aae99e7255697ed791aafa029
jhaskett004 <54286831+jhaskett004@users.noreply.github.com>
2020-11-08 20:01:16 -0600

    Update2

--- app/src/main/java/edu/ualr/intentsassignment/ContactFormActivity.java
+++ app/src/main/java/edu/ualr/intentsassignment/ContactFormActivity.java
@@ -1,14 +1,24 @@
 package edu.ualr.intentsassignment;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
 
+import edu.ualr.intentsassignment.databinding.CfaLayoutBinding;
+
+
 public class ContactFormActivity extends AppCompatActivity {
     //TODO 2. Define the basic skeleton of the ContactFormActivity. Inflate the layout defined in the first step to display the GUI elements on screen.
 
+    private CfaLayoutBinding mBinding;
 
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mBinding = CfaLayoutBinding.inflate(getLayoutInflater());
+    }
 
     // TODO 06. Create a new method that reads the values in the several EditText elements of the layout and then uses the Contact class to send those data to ContactInfoActivity
 }
