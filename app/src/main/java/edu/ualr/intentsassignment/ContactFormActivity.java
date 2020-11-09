2bb1c590402714656ad8d607f5c4fdca15aa1e8d
jhaskett004 <54286831+jhaskett004@users.noreply.github.com>
2020-11-08 23:15:58 -0600

    update

--- app/src/main/java/edu/ualr/intentsassignment/ContactFormActivity.java
+++ app/src/main/java/edu/ualr/intentsassignment/ContactFormActivity.java
@@ -6,29 +6,57 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.content.Context;
 
 import edu.ualr.intentsassignment.databinding.CfaLayoutBinding;
+import edu.ualr.intentsassignment.model.Contact;
+
 import com.google.android.material.button.MaterialButton;
 
 
 public class ContactFormActivity extends AppCompatActivity {
-    //TODO 2. Define the basic skeleton of the ContactFormActivity. Inflate the layout defined in the first step to display the GUI elements on screen.
+    //2. Define the basic skeleton of the ContactFormActivity. Inflate the layout defined in the first step to display the GUI elements on screen.
 
     private CfaLayoutBinding mBinding;
+    private EditText userInput;
+    public static final String EXTRA_CONTACT = "Contact_data";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mBinding = CfaLayoutBinding.inflate(getLayoutInflater());
         setContentView(mBinding.getRoot());
+
+        final MaterialButton button = findViewById(R.id.save_contact_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onButtonClick();
+            }
+        });
     }
 
-    public void onButtonClick(View view) {
+    //06. Create a new method that reads the values in the several EditText elements of the layout and then uses the Contact class to send those data to ContactInfoActivity
+    public void onButtonClick() {
         Intent intent = new Intent(this, ContactInfoActivity.class);
+        Contact contact = new Contact();
+
+        userInput = findViewById(R.id.Fname_textInputLayout);
+        contact.setFirstName(userInput.getText().toString());
+        userInput = findViewById(R.id.Lname_textInputLayout);
+        contact.setLastName(userInput.getText().toString());
+        userInput = findViewById(R.id.telephone_textInputLayout);
+        contact.setPhoneNumber(userInput.getText().toString());
+        userInput = findViewById(R.id.email_textInputLayout);
+        contact.setEmailAddress(userInput.getText().toString());
+        userInput = findViewById(R.id.website_textInputLayout);
+        contact.setWebsite(userInput.getText().toString());
+        userInput = findViewById(R.id.address_textInputLayout);
+        contact.setAddress(userInput.getText().toString());
+
+        intent.putExtra(EXTRA_CONTACT, contact);
+        startActivity(intent);
     }
 
-
-
-
-    // TODO 06. Create a new method that reads the values in the several EditText elements of the layout and then uses the Contact class to send those data to ContactInfoActivity
 }
--- app/src/main/java/edu/ualr/intentsassignment/ContactInfoActivity.java
+++ app/src/main/java/edu/ualr/intentsassignment/ContactInfoActivity.java
@@ -1,26 +1,76 @@
 package edu.ualr.intentsassignment;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import edu.ualr.intentsassignment.databinding.CiaLayoutBinding;
+import edu.ualr.intentsassignment.model.Contact;
+
+import com.google.android.material.chip.Chip;
 
 public class ContactInfoActivity extends AppCompatActivity {
 
     // TODO 04. Define the basic skeleton of the ContactInfoActivity. Inflate the layout defined in the first step to display the GUI elements on screen.
 
+    private CiaLayoutBinding mBinding;
+    private TextView resultTV;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.cia_layout);
+
+        setContactData();
+
+        final Chip callChip = findViewById(R.id.call_chip);
+        callChip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onCallChipClick();
+            }
+        });
+
+
+
+
+
+
+
+    }
+    //07. Create a new method that reads the contact info coming from ContactFormActivity and use it to populate the several TextView elements in the layout.
+    public void setContactData() {
+
+        Contact contact = getIntent().getParcelableExtra(ContactFormActivity.EXTRA_CONTACT);
+
+        resultTV = findViewById((R.id.CIA_full_name));
+        resultTV.setText(contact.getFullName());
+        resultTV = findViewById(R.id.CIA_email_address);
+        resultTV.setText(contact.getEmailAddress());
+        resultTV = findViewById((R.id.CIA_location));
+        resultTV.setText(contact.getAddress());
+        resultTV = findViewById((R.id.CIA_phoneNumber));
+        resultTV.setText(contact.getPhoneNumber());
+        resultTV = findViewById((R.id.CIA_website));
+        resultTV.setText(contact.getWebsite());
+    }
+
+    public void onCallChipClick() {
+        resultTV = findViewById((R.id.CIA_phoneNumber));
+        String phoneNumberUri = String.valueOf(resultTV);
+        Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse(phoneNumberUri));
+
+        startActivity(intent);
     }
 
 
 
 
-
-
-    // TODO 07. Create a new method that reads the contact info coming from ContactFormActivity and use it to populate the several TextView elements in the layout.
     // TODO 08. Create a new method that invokes a Phone Dialer app, using as parameter the phone number included in the contact info received from ContactFormActivity in the previous step
     // TODO 09. Create a new method that invokes a Messages app, using as parameter the phone number included in the contact info received from ContactFormActivity in the 7th step
     // TODO 10. Create a new method that invokes a Maps app, using as parameter the address included in the contact info received from ContactFormActivity in the 7th step
